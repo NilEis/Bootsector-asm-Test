f94049a14f3b77a30fbeef225b33be1901de020f
NilEis <NilEis@users.noreply.github.com>
2020-09-15 18:36:57 +0200

    Update asm.asm

--- asm.asm
+++ asm.asm
@@ -1,5 +1,11 @@
-mov AH, 0x0e
-mov al, 8
+hwString dw "Hello World",0xa
+mov AH, 13h
+mov al, 1
+mov dh, 5
+mov dl, 5
+mov cx, 12
+mov es, [hwString]
+mov bl, 15
 int 0x10
 jmp $
 times 510-($-$$) db 0
