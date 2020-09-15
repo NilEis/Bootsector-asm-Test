5be5010de080f7a69ee49b81752a3f99a6d551ba
NilEis <NilEis@users.noreply.github.com>
2020-09-15 18:59:52 +0200

    Update asm.asm

--- asm.asm
+++ asm.asm
@@ -1,11 +1,17 @@
-hwString dw "Hello World",0xa
+msg  db  "Hello world"
+;Set 80x25 16-color text mode
+xor ah, ah
+mov al, 0x03
+int 0x10
+;-----------
 mov AH, 13h
 mov al, 1
-mov dh, 5
-mov dl, 5
+mov dh, 0
+mov dl, 0
 mov cx, 12
-mov es, [hwString]
-mov bl, 15
+mov es, [0]
+mov bp, msg
+mov bl, 0xf ;Set text-color to white
 int 0x10
 jmp $
 times 510-($-$$) db 0
