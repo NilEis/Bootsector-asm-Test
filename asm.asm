904151b8684088164aeba922088de4d0bb54e79e
NilEis <NilEis@users.noreply.github.com>
2020-09-20 16:47:17 +0200

    Update asm.asm

--- asm.asm
+++ asm.asm
@@ -1,19 +1,8 @@
-msg  db  'Hello world'
-msg_length dw $-msg
 ;Set 80x25 16-color text mode
 xor ah, ah
 mov al, 0x03
 int 0x10
 ;-----------
-mov ax, 0x0a
-mov es, ax
-xor ax,ax
-mov bp, 0x0
-mov al, 1
-mov dh, 0
-mov dl, 0
-mov cx, 11
-mov bl, 0xf ;Set text-color to white
 ;Int 10, 13
 mov ah, 13h
 int 0x10
