c053d14aa16b82c8bd0319607fc542a72a2839f7
NilEis <NilEis@users.noreply.github.com>
2020-09-20 16:48:29 +0200

    Update asm.asm

--- asm.asm
+++ asm.asm
@@ -3,10 +3,30 @@ xor ah, ah
 mov al, 0x03
 int 0x10
 ;-----------
-;Int 10, 13
-mov ah, 13h
+loop:
+;Interrupt read char
+mov ah, 0x00
+int 0x16
+;write
+mov ah, 0x0e
+cmp al,8
 int 0x10
-jmp $
+;cmp
+je clear
+;jmp to top
+jmp loop
+
+backspace:
+
+;clear
+clear:
+mov ah, 0x0e
+mov al, 177
+int 0x10
+mov al, 8
+int 0x10
+jmp loop
+
 times 510-($-$$) db 0
 dw 0xaa55
 times 1474560-($-$$) db 0
