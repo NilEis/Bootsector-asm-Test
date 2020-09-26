76c0c71d07fd3b558d32502e452ead6281da8c03
NilEis <NilEis@users.noreply.github.com>
2020-09-26 15:46:59 +0200

    Update asm.asm

--- asm.asm
+++ asm.asm
@@ -1,23 +1,23 @@
-[org 0x7c00]
+[org 0x7c00]     ; Offset added to all adresses
 
 hwString  db  'Hello world'
 
 ; Set 80x25 16-color text mode
-xor ah, ah   ; set ah = 0
-mov al, 0x03 ; set instruction 0x03
-int 0x10     ; call int 0x10
+xor ah, ah       ; Set ah = 0
+mov al, 0x03     ; Set instruction 0x03
+int 0x10         ; Call int 0x10
 
 ; Write String
-mov ax, 0x0
-mov es, ax
-xor ax,ax
-mov bp, hwString
-mov al, 1 ; 
-mov dh, 0 ; Set row
-mov dl, 0 ; Set column
-mov cx, 11 ; Set String length 
-mov bl, 0xf ; Set text-color to white
-mov ah, 13h ; Set instruction 13h
+mov ax, 0x0      ; ╗
+mov es, ax       ; ╠ Set es = 0 + offset
+xor ax,ax        ; ╝
+mov bp, hwString ; Set bp = hwString + offset
+mov al, 1        ; Set Write mode
+mov dh, 0        ; Set row
+mov dl, 0        ; Set column
+mov cx, 11       ; Set String length
+mov bl, 0xf      ; Set text-color to white
+mov ah, 13h      ; Set instruction 13h
 int 0x10
 jmp $
 times 510-($-$$) db 0
