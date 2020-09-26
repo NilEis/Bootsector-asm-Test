32208fa888f29fe519a06e60082428e33f3e3432
NilEis <NilEis@users.noreply.github.com>
2020-09-26 15:41:42 +0200

    write String

--- asm.asm
+++ asm.asm
@@ -1,32 +1,25 @@
-;Set 80x25 16-color text mode
-xor ah, ah
-mov al, 0x03
-int 0x10
-;-----------
-loop:
-;Interrupt read char
-mov ah, 0x00
-int 0x16
-;write
-mov ah, 0x0e
-cmp al,8
-int 0x10
-;cmp
-je clear
-;jmp to top
-jmp loop
+[org 0x7c00]
 
-backspace:
+hwString  db  'Hello world'
 
-;clear
-clear:
-mov ah, 0x0e
-mov al, 177
-int 0x10
-mov al, 8
-int 0x10
-jmp loop
+; Set 80x25 16-color text mode
+xor ah, ah   ; set ah = 0
+mov al, 0x03 ; set instruction 0x03
+int 0x10     ; call int 0x10
 
+; Write String
+mov ax, 0x0
+mov es, ax
+xor ax,ax
+mov bp, hwString
+mov al, 1 ; 
+mov dh, 0 ; Set row
+mov dl, 0 ; Set column
+mov cx, 11 ; Set String length 
+mov bl, 0xf ; Set text-color to white
+mov ah, 13h ; Set instruction 13h
+int 0x10
+jmp $
 times 510-($-$$) db 0
 dw 0xaa55
 times 1474560-($-$$) db 0
